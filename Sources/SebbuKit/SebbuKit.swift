748a73687b5da1663dd9edd6697613f6b4d2eea1
Sebastian Toivonen <sebastian.m.toivonen@utu.fi>
2020-01-30 19:03:51 +0200

    Some changes

--- Sources/SebbuKit/SebbuKit.swift
+++ Sources/SebbuKit/SebbuKit.swift
@@ -1,12 +1,1 @@
-public struct SebbunStruct {
-    public private(set) var modified: Bool
-    
-    public init() {
-        modified = true
-    }
-}
 
-public func someFunction() -> Int {
-    print("Hello gello")
-    return 42
-}
