09b0cc64a1a1f1efd264d33a6292166fad9202be
Sebastian Toivonen <sebastian.m.toivonen@utu.fi>
2020-01-30 17:49:56 +0200

    Second commit

---
+++ Sources/SebbuKit/BitStreamCoding.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sebastian Toivonen on 30.1.2020.
+//
+
+import Foundation
+
+struct WritableBitStream {
+    
+}
--- Sources/SebbuKit/SebbuKit.swift
+++ Sources/SebbuKit/SebbuKit.swift
@@ -1,3 +1,5 @@
 struct SebbuKit {
     var text = "Hello, World!"
 }
+
+let writeStream = WritableBitStream()
