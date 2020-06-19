e1e38b58ee180e819badd0c9be7ad3a8a61cdb24
Sebastian Toivonen <sebastian.m.toivonen@utu.fi>
2020-06-19 10:33:52 +0300

    package.swift

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/nicklockwood/VectorMath.git", from: "0.4.1"),
     .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0"),
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.0.0")
+    .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
 ]
 
 var targetDependencies: [Target.Dependency] = [
@@ -19,8 +18,7 @@ var targetDependencies: [Target.Dependency] = [
 .product(name: "WebSocketKit", package: "websocket-kit"),
 .product(name: "VectorMath", package: "VectorMath"),
 .product(name: "AsyncHTTPClient", package: "async-http-client"),
-.product(name: "Crypto", package: "swift-crypto"),
-.product(name: "NIOTransportServices", package: "swift-nio-transport-services")]
+.product(name: "Crypto", package: "swift-crypto")]
 
 let package = Package(
     name: "SebbuKit",
@@ -29,7 +27,6 @@ let package = Package(
        .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "SebbuKit", targets: ["SebbuKit"]),
     ],
     
