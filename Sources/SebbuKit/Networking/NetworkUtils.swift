28f22056b934aec4c8ae4ba210381898c51e9b60
Martin <sebastian.toivonen@gmail.com>
2021-02-18 09:47:35 +0200

    Update NetworkUtils.swift

--- Sources/SebbuKit/Networking/NetworkUtils.swift
+++ Sources/SebbuKit/Networking/NetworkUtils.swift
@@ -11,6 +11,61 @@ import Foundation
 import FoundationNetworking
 #endif
 
+extension String {
+    private func isIPv4() -> Bool {
+        let items = self.components(separatedBy: ".")
+        
+        if(items.count != 4) { return false }
+        
+        for item in items {
+            var tmp = 0
+            if(item.count > 3 || item.count < 1){
+                return false
+            }
+            
+            for char in item {
+                if(char < "0" || char > "9"){
+                    return false
+                }
+                
+                tmp = tmp * 10 + Int(String(char))!
+            }
+            
+            if(tmp < 0 || tmp > 255){
+                return false
+            }
+            
+            if((tmp > 0 && item.first == "0") || (tmp == 0 && item.count > 1)){
+                return false
+            }
+        }
+        
+        return true
+    }
+
+    private func isIPv6() -> Bool {
+        let items = self.components(separatedBy: ":")
+        if(items.count != 8){
+            return false
+        }
+        
+        for item in items{
+            if(item.count > 4 || item.count < 1){
+                return false;
+            }
+            
+            for char in item.lowercased() {
+                if((char < "0" || char > "9") && (char < "a" || char > "f")){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    func isIpAddress() -> Bool { return self.isIPv6() || self.isIPv4() }
+}
+
 public struct NetworkUtils {
     private struct IpifyJson: Codable {
         let ip: String
